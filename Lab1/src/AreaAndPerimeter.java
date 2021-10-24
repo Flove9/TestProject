2005ded062c5e7fbdc079817073a4b5283e0f8e5
xenon <xenon@DESKTOP-0K4ET7A>
2021-10-25 00:46:31 +0800

    First commit

---
+++ Lab1/src/AreaAndPerimeter.java
@@ -0,0 +1,13 @@
+
+public class AreaAndPerimeter {
+
+	public static void main(String[] args) {
+		System.out.println("Radius = 6.5");
+		System.out.print("Perimeter = ");
+		System.out.println(2*6.5*3.14159);
+		System.out.print("Area = ");
+		System.out.println(6.5*6.5*3.14159);
+		
+	}
+
+}
